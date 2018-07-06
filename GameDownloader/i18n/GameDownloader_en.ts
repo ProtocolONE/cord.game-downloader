b739a8585e8c0601287c5e91a0652cb913e90736
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2018-07-06 13:35:52 +0300

    Add translation to English

--- GameDownloader/i18n/GameDownloader_en.ts
+++ GameDownloader/i18n/GameDownloader_en.ts
@@ -1,79 +1,77 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
+<!DOCTYPE TS >
 <TS version="2.1" language="en_US">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <source>TITLE_WARNING</source>
-        <translation type="obsolete">Warning</translation>
+      <location filename="../src/GameDownloader/Behavior/CompressorBehavior.cpp" line="120" />
+      <source>COMPRESS_PROGRESS_STATUS_MESSAGE</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <source>INFO_OLD_GAME_CLIENT_MIGRATE</source>
-        <translation type="obsolete">TRANSLATE_ME</translation>
+      <location filename="../src/GameDownloader/Behavior/ExtractorBehavior.cpp" line="114" />
+      <source>EXTRACT_PROGRESS_STATUS_MESSAGE</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <source>ERROR_NO_DISK_SPACE</source>
-        <translation type="obsolete">Not enough space on drive %1</translation>
+      <location filename="../src/GameDownloader/Behavior/ReadOnlyBehavior.cpp" line="71" />
+      <source>FOLDER_PROCESSING_MESSAGE</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <source>PRE_HOOK_DEFAULT_MESSAGE</source>
-        <translation>Prepare for downloading...</translation>
+      <location filename="../src/GameDownloader/GameDownloadService.cpp" line="366" />
+      <location filename="../src/GameDownloader/GameDownloadService.cpp" line="370" />
+      <source>NOT_ENOUGH_SPACE_ON_THE_DISK %1</source>
+      <translation>Not enough space on drive %1</translation>
     </message>
     <message>
-        <source>OLD_GAME_CLIENT_MEGRATE_HOOK</source>
-        <translation type="obsolete">Copy files</translation>
+      <location filename="../src/GameDownloader/Behavior/BindiffBehavior.cpp" line="95" />
+      <source>PATCH_APPLY_BINDIFF_STATUS_MESSAGE</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <source>POST_HOOK_DEFAULT_MESSAGE</source>
-        <translation type="obsolete">Prepare for launch</translation>
+      <location filename="../src/GameDownloader/Behavior/PostHookBehavior.cpp" line="53" />
+      <location filename="../src/GameDownloader/Behavior/PostHookBehavior.cpp" line="65" />
+      <location filename="../src/GameDownloader/Behavior/PreHookBehavior.cpp" line="53" />
+      <location filename="../src/GameDownloader/Behavior/PreHookBehavior.cpp" line="65" />
+      <source>PRE_HOOK_DEFAULT_MESSAGE</source>
+      <translation>Prepare for downloading...</translation>
     </message>
     <message>
-        <source>NOT_ENOUGH_SPACE_ON_THE_DISK %1</source>
-        <translation>Not enough space on drive %1</translation>
+      <location filename="../src/GameDownloader/Hooks/PreventWin32Download.cpp" line="56" />
+      <source>PREVENT_WIN32_LAUNCH_WARNING</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <source>PATCH_APPLY_BINDIFF_STATUS_MESSAGE</source>
-        <translation type="unfinished"></translation>
+      <location filename="../src/GameDownloader/Hooks/PreventWinXpDownload.cpp" line="39" />
+      <source>PREVENT_XP_LAUNCH_WARNING</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <source>REHASH_CURRENT_CLIENT</source>
-        <translation type="unfinished"></translation>
+      <location filename="../src/GameDownloader/Behavior/RehashClientBehavior.cpp" line="62" />
+      <source>REHASH_CURRENT_CLIENT</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <source>EXTRACT_PROGRESS_STATUS_MESSAGE</source>
-        <translation type="unfinished"></translation>
+      <location filename="../src/GameDownloader/Behavior/UninstallBehavior.cpp" line="85" />
+      <source>STATE_PREPARE_UNINSTALL</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <source>COMPRESS_PROGRESS_STATUS_MESSAGE</source>
-        <translation type="unfinished"></translation>
+      <location filename="../src/GameDownloader/Behavior/UninstallBehavior.cpp" line="119" />
+      <source>STATUS_FINISHED</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <source>STATE_PREPARE_UNINSTALL</source>
-        <translation type="unfinished"></translation>
+      <location filename="../src/GameDownloader/Behavior/UninstallBehavior.cpp" line="92" />
+      <source>STATUS_INPROGESS</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <source>STATUS_INPROGESS</source>
-        <translation type="unfinished"></translation>
+      <location filename="../src/GameDownloader/Hooks/PreventWin32Download.cpp" line="55" />
+      <location filename="../src/GameDownloader/Hooks/PreventWinXpDownload.cpp" line="38" />
+      <source>TITLE_ATTENTION</source>
+      <translation type="unfinished" />
     </message>
-    <message>
-        <source>STATUS_FINISHED</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>TITLE_ATTENTION</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>PREVENT_XP_LAUNCH_WARNING</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>PREVENT_WIN32_LAUNCH_WARNING</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>FOLDER_PROCESSING_MESSAGE</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
+  </context>
 </TS>
--- GameDownloader/i18n/GameDownloader_ru.ts
+++ GameDownloader/i18n/GameDownloader_ru.ts
@@ -1,80 +1,77 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
+<!DOCTYPE TS >
 <TS version="2.1" language="ru_RU">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <source>TITLE_WARNING</source>
-        <translation type="obsolete">Внимание</translation>
+      <location filename="../src/GameDownloader/Behavior/CompressorBehavior.cpp" line="120" />
+      <source>COMPRESS_PROGRESS_STATUS_MESSAGE</source>
+      <translation>Установка файлов игры: %1 из %2</translation>
     </message>
     <message>
-        <source>INFO_OLD_GAME_CLIENT_MIGRATE</source>
-        <translatorcomment>Дорогой переводчик. %1 - убьешь это значение - выебу! Он обозначает место подстановки имени игры.</translatorcomment>
-        <translation type="obsolete">На вашем компьютере уже установлена игра %1. Мы можем скопировать игру, чтобы вам не пришлось загружать ее заново.</translation>
+      <location filename="../src/GameDownloader/Behavior/ExtractorBehavior.cpp" line="114" />
+      <source>EXTRACT_PROGRESS_STATUS_MESSAGE</source>
+      <translation>Установка файлов игры: %1 из %2</translation>
     </message>
     <message>
-        <source>ERROR_NO_DISK_SPACE</source>
-        <translation type="obsolete">Недостаточно места на диске %1</translation>
+      <location filename="../src/GameDownloader/Behavior/ReadOnlyBehavior.cpp" line="71" />
+      <source>FOLDER_PROCESSING_MESSAGE</source>
+      <translation>Подготовка папки</translation>
     </message>
     <message>
-        <source>PRE_HOOK_DEFAULT_MESSAGE</source>
-        <translation>Подготовка к запуску...</translation>
+      <location filename="../src/GameDownloader/GameDownloadService.cpp" line="366" />
+      <location filename="../src/GameDownloader/GameDownloadService.cpp" line="370" />
+      <source>NOT_ENOUGH_SPACE_ON_THE_DISK %1</source>
+      <translation>Для установки игры недостаточно места на диске %1</translation>
     </message>
     <message>
-        <source>OLD_GAME_CLIENT_MEGRATE_HOOK</source>
-        <translation type="obsolete">Копирование файлов</translation>
+      <location filename="../src/GameDownloader/Behavior/BindiffBehavior.cpp" line="95" />
+      <source>PATCH_APPLY_BINDIFF_STATUS_MESSAGE</source>
+      <translation>Обновление файлов игры: %1 из %2</translation>
     </message>
     <message>
-        <source>POST_HOOK_DEFAULT_MESSAGE</source>
-        <translation type="obsolete">Подготовка к запуску</translation>
+      <location filename="../src/GameDownloader/Behavior/PostHookBehavior.cpp" line="53" />
+      <location filename="../src/GameDownloader/Behavior/PostHookBehavior.cpp" line="65" />
+      <location filename="../src/GameDownloader/Behavior/PreHookBehavior.cpp" line="53" />
+      <location filename="../src/GameDownloader/Behavior/PreHookBehavior.cpp" line="65" />
+      <source>PRE_HOOK_DEFAULT_MESSAGE</source>
+      <translation>Подготовка к запуску...</translation>
     </message>
     <message>
-        <source>NOT_ENOUGH_SPACE_ON_THE_DISK %1</source>
-        <translation>Для установки игры недостаточно места на диске %1</translation>
+      <location filename="../src/GameDownloader/Hooks/PreventWin32Download.cpp" line="56" />
+      <source>PREVENT_WIN32_LAUNCH_WARNING</source>
+      <translation>Игра %1 не может быть запущена в 32-битной Windows</translation>
     </message>
     <message>
-        <source>COMPRESS_PROGRESS_STATUS_MESSAGE</source>
-        <translation>Установка файлов игры: %1 из %2</translation>
+      <location filename="../src/GameDownloader/Hooks/PreventWinXpDownload.cpp" line="39" />
+      <source>PREVENT_XP_LAUNCH_WARNING</source>
+      <translation>Игра %1 не может быть запущена в Windows XP</translation>
     </message>
     <message>
-        <source>PATCH_APPLY_BINDIFF_STATUS_MESSAGE</source>
-        <translation>Обновление файлов игры: %1 из %2</translation>
+      <location filename="../src/GameDownloader/Behavior/RehashClientBehavior.cpp" line="62" />
+      <source>REHASH_CURRENT_CLIENT</source>
+      <translation>Проверка файлов клиента игры</translation>
     </message>
     <message>
-        <source>REHASH_CURRENT_CLIENT</source>
-        <translation>Проверка файлов клиента игры</translation>
+      <location filename="../src/GameDownloader/Behavior/UninstallBehavior.cpp" line="85" />
+      <source>STATE_PREPARE_UNINSTALL</source>
+      <translation>Подготовка к удалению игры %1</translation>
     </message>
     <message>
-        <source>EXTRACT_PROGRESS_STATUS_MESSAGE</source>
-        <translation>Установка файлов игры: %1 из %2</translation>
+      <location filename="../src/GameDownloader/Behavior/UninstallBehavior.cpp" line="119" />
+      <source>STATUS_FINISHED</source>
+      <translation>Игра %1 успешно удалена</translation>
     </message>
     <message>
-        <source>STATE_PREPARE_UNINSTALL</source>
-        <translation>Подготовка к удалению игры %1</translation>
+      <location filename="../src/GameDownloader/Behavior/UninstallBehavior.cpp" line="92" />
+      <source>STATUS_INPROGESS</source>
+      <translation>Удаление файлов игры %1</translation>
     </message>
     <message>
-        <source>STATUS_INPROGESS</source>
-        <translation>Удаление файлов игры %1</translation>
+      <location filename="../src/GameDownloader/Hooks/PreventWin32Download.cpp" line="55" />
+      <location filename="../src/GameDownloader/Hooks/PreventWinXpDownload.cpp" line="38" />
+      <source>TITLE_ATTENTION</source>
+      <translation>Внимание</translation>
     </message>
-    <message>
-        <source>STATUS_FINISHED</source>
-        <translation>Игра %1 успешно удалена</translation>
-    </message>
-    <message>
-        <source>TITLE_ATTENTION</source>
-        <translation>Внимание</translation>
-    </message>
-    <message>
-        <source>PREVENT_XP_LAUNCH_WARNING</source>
-        <translation>Игра %1 не может быть запущена в Windows XP</translation>
-    </message>
-    <message>
-        <source>PREVENT_WIN32_LAUNCH_WARNING</source>
-        <translation>Игра %1 не может быть запущена в 32-битной Windows</translation>
-    </message>
-    <message>
-        <source>FOLDER_PROCESSING_MESSAGE</source>
-        <translation>Подготовка папки</translation>
-    </message>
-</context>
+  </context>
 </TS>
